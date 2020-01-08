8e4d214227d39c70b63949026d9110f2b394dd3e
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-01-08 00:03:11 -0600

    Se crearon las rutas para las vistas

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,7 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { InventosListComponent } from './components/inventos-list/inventos-list.component';
 import { ReportsComponent } from './components/reports/reports.component';
-
+import { EncuestasListComponent } from './components/encuestas-list/encuestas-list.component';
+import { PaisesListComponent } from './components/paises-list/paises-list.component';
+import { ProfsListComponent } from './components/profs-list/profs-list.component';
+import { InventoresListComponent } from './components/inventores-list/inventores-list.component';
+import { EncuestaFormComponent } from './components/encuesta-form/encuesta-form.component';
+import { PaisFormComponent } from './components/pais-form/pais-form.component';
+import { ProfFormComponent } from './components/prof-form/prof-form.component';
+import { InventorFormComponent } from './components/inventor-form/inventor-form.component';
+import { InventoFormComponent } from './components/invento-form/invento-form.component';
 
 const routes: Routes = [
   {
@@ -13,6 +21,42 @@ const routes: Routes = [
   {
     path: 'reports',
     component: ReportsComponent
+  },
+  {
+    path: 'encuestas',
+    component: EncuestasListComponent
+  },
+  {
+    path: 'nueva-encuesta',
+    component: EncuestaFormComponent
+  },
+  {
+    path: 'paises',
+    component: PaisesListComponent
+  },
+  {
+    path: 'nuevo-pais',
+    component: PaisFormComponent
+  },
+  {
+    path: 'profesionales',
+    component: ProfsListComponent
+  },
+  {
+    path: 'nuevo-profesional',
+    component: ProfFormComponent
+  },
+  {
+    path: 'inventores',
+    component: InventoresListComponent
+  },
+  {
+    path: 'nuevo-inventor',
+    component: InventorFormComponent
+  },
+  {
+    path: 'nuevo-invento',
+    component: InventoFormComponent
   }
 ];
 
