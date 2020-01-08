b9360cc17846aafb2a3cc3d3fb37cff88ceff5bb
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-01-08 00:14:58 -0600

    Modulos de angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,16 @@ import { InventosListComponent } from './components/inventos-list/inventos-list.
 import { ReportsComponent } from './components/reports/reports.component'
 
 import { InventosService } from './services/inventos.service';
-import { ReportsService } from './services/reports.service'
+import { ReportsService } from './services/reports.service';
+import { EncuestasListComponent } from './components/encuestas-list/encuestas-list.component';
+import { PaisesListComponent } from './components/paises-list/paises-list.component';
+import { ProfsListComponent } from './components/profs-list/profs-list.component';
+import { InventoresListComponent } from './components/inventores-list/inventores-list.component';
+import { EncuestaFormComponent } from './components/encuesta-form/encuesta-form.component';
+import { PaisFormComponent } from './components/pais-form/pais-form.component';
+import { ProfFormComponent } from './components/prof-form/prof-form.component';
+import { InventorFormComponent } from './components/inventor-form/inventor-form.component';
+import { InventoFormComponent } from './components/invento-form/invento-form.component'
 
 
 @NgModule({
@@ -17,12 +27,22 @@ import { ReportsService } from './services/reports.service'
     AppComponent,
     NavbarComponent,
     InventosListComponent,
-    ReportsComponent
+    ReportsComponent,
+    EncuestasListComponent,
+    PaisesListComponent,
+    ProfsListComponent,
+    InventoresListComponent,
+    EncuestaFormComponent,
+    PaisFormComponent,
+    ProfFormComponent,
+    InventorFormComponent,
+    InventoFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [ 
     InventosService,
